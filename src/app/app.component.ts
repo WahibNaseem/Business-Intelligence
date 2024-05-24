a3dfb72c3d8d03bdb0dd09d16598caaef66f3b15
Wahib Naseem <wahib.naseem@seppmed.de>
2024-05-24 11:05:54 +0200

    Add Js dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'BusinessIntelligence';
+  title = 'Hallo von Business Intelligence';
 }
